286263f5f084ce2f689c349e542684649e18ca7f
Kostiantyn Verbytskyi <kosver0427@gmail.com>
2024-01-16 16:02:08 +0200

    Add editing page

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,14 +26,42 @@ namespace BulkyWeb.Controllers
 		public IActionResult Create(Category obj)
 		{
 			//custom validation text
-			//if (obj.Name != null && obj.Name == obj.DisplayOrder.ToString())
-			//{
-			//	ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
-			//}
+			if (obj.Name != null && obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+			}
 
 			if (ModelState.IsValid)
 			{
 				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category categoryFromDb = _db.Categories.Find(id);
+			//the same things
+			//Category categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+			//Category categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+			if (categoryFromDb==null)
+            {
+				return NotFound();
+            }
+            return View(categoryFromDb);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
 				_db.SaveChanges();
 				return RedirectToAction("Index");
 			}
