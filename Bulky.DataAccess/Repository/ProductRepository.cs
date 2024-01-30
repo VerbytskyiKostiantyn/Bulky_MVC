3c809a2dbb469dcdc91d92be502601c6df6058e9
Verbytskyi Kostiantyn <kosver0427@gmail.com>
2024-01-30 22:06:27 +0200

    update the update method in product and other bugfixes

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -10,16 +10,32 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class ProductRepository : Repository<Product>, IProductRepository
-    {
-        public ApplicationDBContext _db { get; set; }
-        public ProductRepository(ApplicationDBContext db) : base(db)
-        {
-            _db = db;
-        }
-        public void Update(Product obj)
-        {
-            _db.Products.Update(obj);
-        }
-    }
+	public class ProductRepository : Repository<Product>, IProductRepository
+	{
+		public ApplicationDBContext _db { get; set; }
+		public ProductRepository(ApplicationDBContext db) : base(db)
+		{
+			_db = db;
+		}
+		public void Update(Product obj)
+		{
+			var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+			if (objFromDb != null)
+			{
+				objFromDb.Title = obj.Title;
+				objFromDb.Description = obj.Description;
+				objFromDb.CategoryId = obj.CategoryId;
+				objFromDb.ISBN = obj.ISBN;
+				objFromDb.Author = obj.Author;
+				objFromDb.ListPrice = obj.ListPrice;
+				objFromDb.Price = obj.Price;
+				objFromDb.Price50 = obj.Price50;
+				objFromDb.Price100 = obj.Price100;
+				if (obj.ImageUrl != null)
+				{
+					objFromDb.ImageUrl = obj.ImageUrl;
+				}
+			}
+		}
+	}
 }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -86,13 +86,15 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 				if (productVM.Product.Id == 0)
 				{
 					_unitOfWork.Product.Add(productVM.Product);
+					_unitOfWork.Save();
+					TempData["success"] = "Product created successfully";
 				}
 				else
 				{
 					_unitOfWork.Product.Update(productVM.Product);
+					_unitOfWork.Save();
+					TempData["success"] = "Product updated successfully";
 				}
-				_unitOfWork.Save();
-				TempData["success"] = "Product created successfully";
 				return RedirectToAction("Index");
 			}
 			else
