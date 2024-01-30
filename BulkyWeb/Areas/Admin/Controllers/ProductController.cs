ac8dc2932aec63e375dd0c796ff4f4b3c2165ab7
Verbytskyi Kostiantyn <kosver0427@gmail.com>
2024-01-30 20:46:14 +0200

    update the create product functionality

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -10,11 +10,12 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProductController : Controller
     {
-        public IUnitOfWork _unitOfWork { get; set; }
-
-        public ProductController(IUnitOfWork unitOfWork)
+        private IUnitOfWork _unitOfWork { get; set; }
+		private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+			_webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -57,6 +58,19 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 		{
 			if (ModelState.IsValid)
 			{
+				string wwwRootPath = _webHostEnvironment.WebRootPath;
+				if (file != null)
+				{
+					string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+					string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+					using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+					{
+						file.CopyTo(fileStream);
+					}
+
+					productVM.Product.ImageUrl = @"\images\product\" + fileName;
+				}
 				_unitOfWork.Product.Add(productVM.Product);
 				_unitOfWork.Save();
 				TempData["success"] = "Product created successfully";
