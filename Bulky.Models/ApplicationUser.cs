a2b3c741a856cfe74d4b40ebbe0b97a6a5508c7a
Verbytskyi Kostiantyn <kosver0427@gmail.com>
2024-02-26 23:11:31 +0200

    add base implementation of manage users page

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -18,10 +18,12 @@ namespace BulkyBook.Models
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
-        public string? PostalCode { get; set;}
+        public string? PostalCode { get; set; }
         public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
+        [NotMapped]
+        public string Role {  get; set; }  
     }
 }
